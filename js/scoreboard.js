99f4b45f58213c905086fdfb606ea71e73406506
Giuseppe Canale <20385856+xbeat@users.noreply.github.com>
2018-03-10 12:05:41 +0100

    Update

---
+++ js/scoreboard.js
@@ -0,0 +1,74 @@
+function managePlayerInfo(){
+	document.getElementsByClassName( "playerInfo" )[ 0 ].classList.toggle( "playerInfoHide" );
+};
+
+function manageScoreboard(){
+	document.getElementsByClassName( "scoreboardTimer" )[ 0 ].classList.toggle( "scoreboardTimerHide" );
+};
+
+function managePitch(){
+	document.getElementById( "canvasPitch" ).classList.toggle( "canvasPitch" );
+};
+
+/*
+* Timer
+*/
+class ScoreboardTimer{
+	constructor( callback ) {
+		this.time = 5400;
+		this.status = 0;
+		this.timer_id;
+		this.callback = callback;
+		this.reset();
+	};
+
+	start() {
+		if( this.status == 0 ) {
+			this.status = 1;
+			this.timer_id = setInterval( function() {
+				this.time--;
+				this.generateTime();
+				if( typeof( this.callback ) === 'function') this.callback( this.time );
+			}.bind( this ), 1000);
+		};
+	};
+
+	stop() {
+		if( this.status == 1 ){
+			this.status = 0;
+			clearInterval( this.timer_id );
+		};
+	};
+
+	reset()	{
+		this.time = 5400;
+		this.generateTime();
+	};
+
+	getTime() {
+		return this.time;
+	};
+
+	getStatus()	{
+		return this.status;
+	};
+
+	generateTime() {
+		this.second = this.time % 60;
+		this.minute = Math.floor( this.time / 60 ) % 60;
+		this.second = ( this.second < 10 ) ? '0' + this.second : this.second;
+		this.minute = ( this.minute < 10 ) ? '0' + this.minute : this.minute;
+		document.getElementsByClassName('second')[ 0 ].innerText = this.second;
+		document.getElementsByClassName('minute')[ 0 ].innerText = this.minute;
+	};
+}
+
+
+let	scoreboardTimer = new ScoreboardTimer(
+	function( time ) {
+		if( time <= 0 ) { 
+			timer.stop();
+			alert('time out');
+		};
+	}
+);
